686ca4cfb849e42a997d70b0dd5dfc8627673d67
Thomas Dupre <thomasdpr-pro@outlook.com>
2023-09-09 01:09:02 +0200

    Update portfolio

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,40 +1,49 @@
 import React, { useState } from 'react'
+
 import { Link } from 'react-router-dom';
 import hamburger from '../../assets/icons/hamburger.svg';
 import logo from '../../assets/icons/logo-symbol.svg';
-import scrolledLogo from '../../assets/icons/logo-text.svg';
 
 import './Navbar.scss'
 
 export const Navbar = () => {
 
-  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  // Cette fonction permet de gérer l'état du menu burger (ouvert ou fermé)
+  const [showNavbar, setShowNavbar] = useState(false)
 
-  const toggleMenu = () => {
-    setIsMenuOpen(!isMenuOpen);
+  // Cette fonction permet de changer l'état du menu burger (ouvert ou fermé)
+  const handleShowNavbar = () => {
+    setShowNavbar(!showNavbar)
   }
 
   return (
-    <nav className={`navbar ${isMenuOpen ? "navbar--default" : "navbar"}`}>
-      <div className="navbar__logo">
-        <Link to='/'>
-          <img src={logo} className="navbar__logo__image" alt="logo" />
-        </Link>
+    <nav className="navbar">
+      <div className="navbar__container">
+
+        <div className="navbar__logo">
+          <Link to='/'>
+            <img src={logo} className="navbar__logo__image" alt="logo" />
+          </Link>
+        </div>
+
+        <div className="navbar__hamburger">
+          <img src={hamburger} className="navbar__hamburger__image" alt="menu" onClick={handleShowNavbar} />
+        </div>
+
+        <div className={`navbar__elements ${showNavbar && 'active'}`}>
+          <ul className="navbar__elements__links" onClick={handleShowNavbar}>
+            <li>
+              <Link to='/projects' className="navbar__elements__link">Mes Projets</Link>
+            </li>
+            <li>
+              <Link to='/skills' className="navbar__elements__link">Mes compétences</Link>
+            </li>
+            <li>
+              <Link to='/contact' className="navbar__elements__link">Me contacter</Link>
+            </li>
+          </ul>
+        </div>
       </div>
-      <ul className="navbar__links">
-        <li className="navbar__item">
-          <Link to='/projects' className="navbar__link" onClick={toggleMenu}>Mes Projets</Link>
-        </li>
-        <li className="navbar__item">
-          <Link to='/skills' className="navbar__link" onClick={toggleMenu}>Mes compétences</Link>
-        </li>
-        <li className="navbar__item">
-          <Link to='/contact' className="navbar__link" onClick={toggleMenu}>Me contacter</Link>
-        </li>
-      </ul>
-      <button className="navbar__burger" onClick={toggleMenu}>
-        <img className="navbar__burger__image"src={hamburger} alt="menu" />
-      </button>
     </nav>
   )
 }
