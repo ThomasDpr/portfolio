23719660ab31ced861249327f37347ef4dbf7dd7
Thomas Dupre <thomasdpr-pro@outlook.com>
2023-09-10 12:36:13 +0200

    Update portfolio

--- src/components/HeroSection/HeroSection.tsx
+++ src/components/HeroSection/HeroSection.tsx
@@ -4,9 +4,9 @@ import avatar from "../../assets/images/avatar-header.png";
 
 export const HeroSection = () => {
   return (
-    <div className="header">
+    <header className="header">
 
-      <div className="header__text">
+      <section className="header__text">
         <h1 className="header__text__title">Thomas Dupré</h1>
         <h2 className="header__text__subtitle">Développeur Full Stack</h2>
         <p className="header__text__description">
@@ -15,12 +15,12 @@ export const HeroSection = () => {
           le développement front-end, mais je suis également à l'aise avec le
           back-end.
         </p>
-      </div>
+      </section>
 
-      <div className="header__image">
-        <img src={avatar} alt="avatar" />
-      </div>
+      <figure className="header__image">
+        <img src={avatar} alt="Portrait de Thomas Dupré" />
+      </figure>
 
-    </div>
+    </header>
   );
 };
