570b294b618a0eb81de1c901962b5570e14349d7
Thomas Dupre <thomasdpr-pro@outlook.com>
2023-09-10 16:44:35 +0200

    Update portfolio

--- src/components/Banner/Banner.tsx
+++ src/components/Banner/Banner.tsx
@@ -2,34 +2,43 @@ import React from "react";
 import Marquee from "react-fast-marquee";
 import "./Banner.scss";
 
-export const Banner: React.FC = () => {
+import code from "../../assets/icons/code.svg";
+import responsive from "../../assets/icons/responsive.svg";
+import performance from "../../assets/icons/performance.svg";
+import webDesign from "../../assets/icons/web-design.svg";
+
+export const Banner = () => {
+
   const items = [
-    "Web Design",
-    "App Design",
-    "Front-End",
-    "UI/UX",
-    "Back-End",
-    "Full Stack",
+    { text: "Clean Code", icon: code },
+    { text: "Responsive Design", icon: responsive },
+    { text: "Optimisation de la performance web", icon: performance },
+    { text: "Intérêt pour le Design UX/UI", icon: webDesign },
   ];
 
   const marqueeProps = {
-    gradient: false,
     speed: 100,
     pauseOnHover: true,
     pauseOnClick: true,
     pauseOnVisibilityChange: true,
     gradientWidth: 20,
+    autoFill: true,
+    gradient: true,
+    // play:false,
   };
 
   return (
     <div className="banner">
       <div className="banner__container">
-        <Marquee {...marqueeProps} gradientColor={[0,0,0]} gradient={true}>
-          <div className="banner__container__text">
+        <Marquee {...marqueeProps} gradientColor={[0,0,0]}>
             {items.map((item, index) => (
-              <span className="banner__container__text__item" key={item}>{item}</span>
-            ))}
+          <div className="banner__container__text" key={index}>
+              <img className="banner__container__text__icon" src={item.icon} alt="icon" />
+              <span className="banner__container__text__item">
+                {item.text}
+              </span>
           </div>
+            ))}
         </Marquee>
       </div>
     </div>
