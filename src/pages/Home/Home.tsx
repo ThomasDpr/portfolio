763c68d2eb4a804e5d9bd0021af6e5f6f803de0d
Thomas Dupre <thomasdpr-pro@outlook.com>
2023-09-13 11:49:14 +0200

    Update portfolio

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { HeroSection } from '../../components/HeroSection/HeroSection';
-// import { Banner } from '../../components/Banner/Banner';
 import { ProjectSection } from '../../components/ProjectsSection/ProjectSection';
 import { SkillsSection } from '../../components/SkillsSection/SkillsSection';
 import { Links } from '../../components/Links/Links';
@@ -11,10 +10,9 @@ export const Home = () => {
   return (
     <div className="home">
       <HeroSection />
-      {/* <Banner /> */}
       <ProjectSection />
-      {/* <SkillsSection />
-      <Links /> */}
+      <SkillsSection />
+      <Links />
     </div>
   );
 }
