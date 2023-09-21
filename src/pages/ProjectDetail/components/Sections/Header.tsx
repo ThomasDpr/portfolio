da047294c75f884f6c55fd7e093605b15ef245ea
Thomas Dupre <thomasdpr-pro@outlook.com>
2023-09-21 14:59:59 +0200

    update portfolio

--- src/pages/ProjectDetail/components/Sections/Header.tsx
+++ src/pages/ProjectDetail/components/Sections/Header.tsx
@@ -2,13 +2,12 @@ import './Header.scss';
 
 export const Header = ({ title, imageSrc }: { title: string, imageSrc: string }) => (
   <section className="project-header">
-    <section className="project-header__title">
-      <h1 className="project-header__title__text">{title}</h1>
-    </section>
-
-
     <figure className="project-header__image">
       <img className="project-header__image__img" src={imageSrc} alt={`Illustation de ${title}`} />
     </figure>
+
+    <section className="project-header__title">
+      <h1 className="project-header__title__text">{title}</h1>
+    </section>
   </section>
 );
